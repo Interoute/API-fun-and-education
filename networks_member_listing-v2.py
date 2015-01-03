3f5785300c9353557eb07d4309adb085915e1fe0
Phillip Kent <phillip.kent@gmail.com>
2015-01-03 21:17:24 +0000

    working on new version of networks_member_listing

--- networks_member_listing-v2.py
+++ networks_member_listing-v2.py
@@ -13,6 +13,7 @@ import getpass
 import json
 import os
 import string
+import datetime
 
 
 if __name__ == '__main__':
@@ -40,49 +41,70 @@ if __name__ == '__main__':
     api = vdc.VDCApiCall(api_url, apiKey, secret)
 
     #API calls to get the information about networks and VMs
-    requests = {}
-    networksList = api.listNetworks(requests)
-    vmList = api.listVirtualMachines(requests)
+    request = {}
+    networksList = api.listNetworks(request)
+    vmList = api.listVirtualMachines(request)
+    portForwardingRulesList = api.listPortForwardingRules(request)
 
     nameStringSubs = string.maketrans(" -","__")
 
     try:
         diagfile = open('VDC-network-data.diag','w')
+        checkTime = datetime.datetime.utcnow() # get the current time (UTC = GMT)
         diagfile.write('nwdiag {\n')
-        print(" "+unichr(0x2502))
+        print("\nNetwork configuration for the account '%s' checked at %s:" % (vmList['virtualmachine'][0]['account'],checkTime.strftime("%Y-%m-%d %H:%M:%S UTC")))
         for network in networksList['network']:
-            print(" "+unichr(0x251C)+' %s (CIDR: %s, Zone: %s' % (
+            print(" "+unichr(0x2015)+' \'%s\' (CIDR: %s, Zone: %s' % (
                 network['name'],
                 network['cidr'],
                 network['zonename']
             ), end='')
             #FIND EXTERNAL IP ADDRESS IF EXISTS
-            testdict=requests
+            testdict=request
             testdict['associatednetworkid']=network['id']
             external_IP=api.listPublicIpAddresses(testdict)
             if external_IP != {}:
-               print(", IP: %s)" % external_IP['publicipaddress'][0]['ipaddress']) 
+               print(", IP: %s)" % external_IP['publicipaddress'][0]['ipaddress'])
             else:
                print(")")
             members = []
             for vm in vmList['virtualmachine']:
                 for i in range(len(vm['nic'])):
                     if network['id'] == vm['nic'][i]['networkid']:
-                        members.append([int(vm['nic'][i]['ipaddress'].split('.')[-1]),vm['nic'][i]['ipaddress'],vm['name']])
-                        break # Can break out of this loop as soon as the network is found in one of the NICs
+                        members.append([int(vm['nic'][i]['ipaddress'].split('.')[-1]),vm['nic'][i]['ipaddress'],vm['name'],vm['id']])
+                        break # Can break out of this loop as soon as the network id is found for a NIC 
             if len(members)>0:
                 diagfile.write('network %s {\n address=\"%s\"\n' % (str(network['name']).translate(nameStringSubs),network['cidr']))
                 members.sort() # VMs will be sorted by the last segment of their IP address (=first element of each members list)
                 for i in range(len(members)):
                     if i==len(members)-1:  #this is last VM in the network
-                       print(" "+unichr(0x2502)+"   "+unichr(0x2514)+" %s: %s" % (members[i][1],members[i][2]))
+                       print("   "+unichr(0x2514)+" %s: '%s'" % (members[i][1],members[i][2]), end='')
                     else:
-                       print(" "+unichr(0x2502)+"   "+unichr(0x251C)+" %s: %s" % (members[i][1],members[i][2]))
+                       print("   "+unichr(0x251C)+" %s: '%s'" % (members[i][1],members[i][2]), end='')
+                    if external_IP != {}:
+                       #check for port forwarding rules
+                       ##for p in portForwardingRulesList['portforwardingrule']:
+                           ##print( "check vm id: %s == %s (%s)   check ip: %s == %s (%s)" % (p['virtualmachineid'],members[i][3], p['virtualmachineid']==vm['id'],p['ipaddress'],external_IP['publicipaddress'][0]['ipaddress'],p['ipaddress']==external_IP['publicipaddress'][0]['ipaddress']))
+                       pfRules = [p for p in portForwardingRulesList['portforwardingrule'] if p['virtualmachineid']==members[i][3] and p['ipaddress']==external_IP['publicipaddress'][0]['ipaddress']]
+                       if pfRules != []:
+                          print(" (ports:",end='')
+                          for p in pfRules:
+                              ##print(p)
+                              if p['publicport']!=p['publicendport']:
+                                 print(" [%s/%s]->"%(p['publicport'],p['publicendport']),end='')
+                              else:
+                                 print(" [%s]->"%(p['publicport']),end='')
+                              if p['privateport']!=p['privateendport']:
+                                 print("[%s/%s]"%(p['privateport'],p['privateendport']),end='')
+                              else:
+                                 print("[%s]"%(p['privateport']),end='')
+                          print(")",end='')
+                    print(" ")
                     diagfile.write('\"%s\" [address=\"%s\"];\n' % (str(members[i][2]).translate(nameStringSubs),members[i][1]))
                 diagfile.write('}\n')
             else:
-                print(" "+unichr(0x2502)+"   *(NO MEMBERS)")
-            print(" "+unichr(0x2502))
+                print("   *(NO MEMBERS)")
+            print(" ")
         diagfile.write("}\n")
         diagfile.close()
                 
