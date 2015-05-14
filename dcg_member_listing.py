407967f7238db7d3238cfe69b14b47ee8aa0f76a
Phillip Kent <phillip.kent@xmlsoup.com>
2015-05-14 16:41:52 +0100

    work in progress on dcg_member_listing.py

--- dcg_member_listing.py
+++ dcg_member_listing.py
@@ -20,20 +20,33 @@ import datetime
 import argparse
 import re
 
+def print_network_members(vmList,networkid,prefixChars):
+   networkmembers = []
+   for vm in vmList['virtualmachine']:
+      for i in range(len(vm['nic'])):
+         if networkid == vm['nic'][i]['networkid']:
+            networkmembers.append([int(vm['nic'][i]['ipaddress'].split('.')[-1]),vm['nic'][i]['ipaddress'],vm['name'],vm['id']])
+            break # Can break out of this loop as soon as the network id is found for a NIC 
+   if len(networkmembers)>0:
+      networkmembers.sort() # VMs will be sorted by the last segment of their IP address (=first element of each members list)
+      for i in range(len(networkmembers)):
+         if i==len(networkmembers)-1:  #this is last VM in the network
+            print(prefixChars + unichr(0x2514)+" %s: '%s'" % (networkmembers[i][1],networkmembers[i][2]))
+         else:
+            print(prefixChars + unichr(0x251C)+" %s: '%s'" % (networkmembers[i][1],networkmembers[i][2]))
+   else:
+      print(prefixChars + "*(NO MEMBERS)")
+
+
 if __name__ == '__main__':
     # STEP 1: Parse command line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--config", default=os.path.join(os.path.expanduser('~'), '.vdcapi'),
                     help="path/name of the config file to be used for the API URL and API keys")
-    parser.add_argument("-n", "--netmem",help="show the VM members of private direct connect networks")
-    ##parser.add_argument("-f", "--diagfile", default='VDC-network-data.diag',
-    ##                help="name of the output diag file for use with nwdiag")
-    ##parser.add_argument("-z", "--zone",help="filter results by zone name (match by initial characters) ")
-    ##diagfileName = parser.parse_args().diagfile
+    parser.add_argument("-n","--netmem",action='store_true',
+                    help="show the VM members of the Private Direct Connect networks")
     config_file = parser.parse_args().config
-    ##show_netmem = True
-    ##zonenameFilter = parser.parse_args().zone
-
+    show_netmem = parser.parse_args().netmem
 
     # STEP 2: If config file is found, read its content,
     # else query user for the URL, API key, Secret key
@@ -58,8 +71,8 @@ if __name__ == '__main__':
     vdcRegions = ['Europe', 'USA', 'Asia']
     dcgList = api.listDirectConnectGroups({})
     networksLists = {}
-    ##if show_netmem:
-    ##   vmLists = {}
+    if show_netmem:
+       vmLists = {}
     for r in vdcRegions:
        networksLists[r] = api.listNetworks({'region': r, 'subtype': 'privatedirectconnect'})
 
@@ -68,7 +81,6 @@ if __name__ == '__main__':
         checkTime = datetime.datetime.utcnow() # get the current time (UTC = GMT)
         print("\nDirect Connect Groups for the account '%s' checked at %s:"
             % (networksLists['Europe']['network'][0]['domain'], checkTime.strftime("%Y-%m-%d %H:%M:%S UTC")))
-        ##print("\nDirect Connect Groups checked at %s:" % (checkTime.strftime("%Y-%m-%d %H:%M:%S UTC")))
         for d in dcgList['directconnectgroups']:
             print(" "+unichr(0x2015)+' \'%s\' (dcgid: %s)' % (
                 d['name'],
@@ -76,8 +88,11 @@ if __name__ == '__main__':
             ))
             members = []
             for r in vdcRegions:
-               ##if show_netmem:
-               ##   vmLists[r] = api.listVirtualMachines({'region':r})        
+               if show_netmem:
+                  zonesResponse = api.listZones({'region':r})
+                  zonesList = [[z['id'],z['name']] for z in zonesResponse['zone']]
+                  for z in zonesList:
+                     vmLists[z[1]] = api.listVirtualMachines({'region':r,'zoneid':z[0]})        
                for n in networksLists[r]['network']:
                   if n['dcgfriendlyname'] == d['name']:
                      members.append([n['cidr'],n['name'],n['zonename'],r,n['id']])
@@ -87,9 +102,18 @@ if __name__ == '__main__':
                 for i in range(len(members)):
                     if i==len(members)-1:  #if this is last item in list
                        print("   "+unichr(0x2514)+" %s: '%s' (%s, %s)" % (members[i][0],members[i][1],members[i][2],members[i][3]))
+                       if show_netmem:
+                           if vmLists[members[i][2]] != {}:
+                              print_network_members(vmLists[members[i][2]],members[i][4],"        ")
+                           else:
+                              print("        " + "*(NO MEMBERS)")
                     else:
                        print("   "+unichr(0x251C)+" %s: '%s' (%s, %s)" % (members[i][0],members[i][1],members[i][2],members[i][3]))
-                       ##if show_netmem:
+                       if show_netmem:
+                           if vmLists[members[i][2]] != {}:
+                              print_network_members(vmLists[members[i][2]],members[i][4],"   "+unichr(0x2502)+"    ")
+                           else:
+                              print("   " + unichr(0x2502) + "    " + "*(NO MEMBERS)")
 
                 print(" ")
             else:
