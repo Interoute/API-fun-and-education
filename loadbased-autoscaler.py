1aa8686e57c82e7db6915153610e3e3db268d2f7
Phillip Kent <phillip.kent@xmlsoup.com>
2016-08-09 19:12:54 +0100

    loadbased-autoscaler.py: work in progress

--- loadbased-autoscaler.py
+++ loadbased-autoscaler.py
@@ -8,9 +8,12 @@
 # You can pass options via the command line: type 'python loadbased-autoscaler.py -h'
 # for usage information
 #
-# Based on original VDC design and Perl script by Stefan Bienek
+# Based on an original VDC implementation and Perl script by Stefan Bienek
 #
 # Copyright (C) Interoute Communications Limited, 2016
+#
+# Reference: 
+#   http://www.kloppmagic.ca/auto-scaling-with-haproxy/
 
 from __future__ import print_function
 import vdc_api_call as vdc
@@ -25,7 +28,7 @@ import datetime
 import argparse
 import re
 import math
-import random
+from subprocess import call
 
 if __name__ == '__main__':
     # STEP 1: Parse command line arguments
@@ -51,23 +54,25 @@ if __name__ == '__main__':
     haproxyStatsURL = 'http://213.39.4.189:8080/haproxy?stats;csv'
     haproxyStatsUser = 'stats'
     haproxyStatsPassword = '132stats'
+    haproxyConfigFileStatic = 'haproxy_cfg_static'
+    haproxyConfigFile = '/etc/haproxy/haproxy.cfg'
     zoneID = '7144b207-e97e-4e4a-b15d-64a30711e0e7'
     templateID = '545abbf5-5d37-4156-8c4f-85571f1bd5b3'
     serviceofferingID = '4cb92069-e001-4637-8848-76d74f406bb8'
     networkIDs = 'e7d5c36d-ff6f-42c1-bbc3-8fd83a2b97f4,82d6f7cb-d654-4a5e-949b-8fecb8f66c04'
-    # naming convention for VMs, a date and random digits will be added
+    # naming convention for VMs, a timestamp and count digits will be added
     vmName = 'Webcluster-Frankfurt-web-'   
     # min and max numbers of VMs to run
     maxVM = 10			
     minVM = 1
-    # max allowed loading of cluster is a number of web sessions per VM
+    # max allowed loading of cluster is a mean number of web sessions per VM
     sessionPerVM = 5		
 
     # STEP 2: If config file is found, read its content,
     # else query user for the URL, API key, Secret key
     if os.path.isfile(config_file):
         with open(config_file) as fh:
-            data = fh.read()
+            data = fh.read()            
             config = json.loads(data)
             api_url = config['api_url']
             apiKey = config['api_key']
@@ -91,14 +96,17 @@ if __name__ == '__main__':
 
     # STEP: Check the number of running VMs
     vmresult1 = api.listVirtualMachines({'region': vdcRegion, 'zone': zoneID, 'state': 'Running', 'name': vmName})
-    ## Probably need to also add VMs with state=Starting because the VM deploy time could be longer than the run frequency of this script
+    ## add VMs with state=Starting because the VM deploy time could be longer than the run frequency of this script
     vmresult2 = api.listVirtualMachines({'region': vdcRegion, 'zone': zoneID, 'state': 'Starting', 'name': vmName})
-    if vmresult2 != {}:
+    if vmresult2 != {} and vmresult1 != {}:
        currentVmCount = vmresult1['count'] + vmresult2['count']
        currentVmList = [[vm['id'],vm['name'],vm['state']] for vm in vmresult1['virtualmachine']] + [[vm['id'],vm['name'],vm['state']] for vm in vmresult2['virtualmachine']]
-    else:
+    elif vmresult1 != {}:
        currentVmCount = vmresult1['count']
        currentVmList = [[vm['id'],vm['name'],vm['state']] for vm in vmresult1['virtualmachine']]
+    else:
+       currentVmCount = 0
+       currentVmList = []
     # sort list alphabetically by VM names
     currentVmList.sort(key=lambda x: x[1])
     print("Current virtual machines: %d" % currentVmCount)
@@ -107,7 +115,7 @@ if __name__ == '__main__':
         print("%s: %s (state: %s)" % (v[0],v[1],v[2]))
     
     # STEP: Query to HAProxy VM statistics and extract the number of current sessions
-    # Do this twice to get a more reliable number
+    # Do this twice with a time gap to get a more reliable number
     haproxydata = requests.get(haproxyStatsURL, auth=(haproxyStatsUser, haproxyStatsPassword)).text
     currentSessions1 = float((haproxydata.split('\n')[1]).split(',')[4])
     time.sleep(10)
@@ -122,7 +130,8 @@ if __name__ == '__main__':
     else:
        requiredVmCount = currentSessionsCount / sessionPerVM + 1
 
-    requiredVmCount = 5
+    # TEMPORARY CONTROL!
+    requiredVmCount = 3
 
     if requiredVmCount > maxVM:
        requiredVmCount = maxVM
@@ -130,11 +139,8 @@ if __name__ == '__main__':
        requiredVmCount = minVM
 
     print("Required VMs (for maximum loading of %d sessions per VM): %d" % (sessionPerVM, requiredVmCount))
-        
     changeVMNum = requiredVmCount - currentVmCount
-
     print("Change to VMs: %d" % changeVMNum)
-
 
     # STEP: Create new VM, or delete VM, or no changes required
     if changeVMNum == 0:
@@ -143,18 +149,64 @@ if __name__ == '__main__':
        print("Creating %d VMs now..." % changeVMNum)
        timeNow = datetime.datetime.utcnow()
        timestamp = timeNow.strftime("%Y%m%dT%H%M%S")
+       newVmDict = {}
        for i in range(changeVMNum):
-          ## vmNewName = vmName + timestamp + '-' + "%04d" % int(random.random()*10000)
           vmNewName = vmName + timestamp + '-' + "%03d" % (i+1)
           print("  creating: %s" % vmNewName)
-          api.deployVirtualMachine({'region':vdcRegion, 'name':vmNewName, 'displayname':vmNewName, 'zoneid': zoneID, 'templateid': templateID, 'serviceofferingid': serviceofferingID, 'networkids': networkIDs})       
+          newVmDict[vmNewName] = {}
+          deployResult = api.deployVirtualMachine({'region':vdcRegion, 'name':vmNewName, 'displayname':vmNewName, 'zoneid': zoneID, 'templateid': templateID, 'serviceofferingid': serviceofferingID, 'networkids': networkIDs}) 
+          newVmDict[vmNewName]['deployjobid'] = deployResult['jobid']
+          newVmDict[vmNewName]['deploycomplete'] = False
+       # NEED TO WAIT FOR DEPLOYS TO COMPLETE SO THAT NEW VM'S IP ADDRESS IS AVAILABLE TO HAPROXY CONFIG 
+       checkDelay = 2
+       displayProgress = True
+       deployAllComplete = False
+       countdown = len(newVmDict)
+       while not deployAllComplete:
+          for v in newVmDict: 
+              if not newVmDict[v]['deploycomplete']:
+                 try:
+                    result = api.queryAsyncJobResult({'region': vdcRegion, 'jobid': newVmDict[v]['deployjobid']})
+                 except KeyError:
+                    pass
+                 if 'jobresult' in result:
+                    countdown = countdown - 1
+                    newVmDict[v]['deploycomplete'] = True
+                    print('')
+                    print("VM deploy completed: %s. %d deploys left to complete." % (v, countdown))
+          if countdown == 0:
+             deployAllComplete = True
+             print("Finished the deployment of virtual machines.")
+          else:
+             if displayProgress:
+                print('.', end='')
+                sys.stdout.flush()
+             time.sleep(checkDelay)       
     elif changeVMNum < 0:
        print("Deleting %d VMs now..." % abs(changeVMNum)) 
        for i in range(abs(changeVMNum)):
           print("  deleting: %s" % currentVmList[i][1])
           api.destroyVirtualMachine({'region':vdcRegion, 'id': currentVmList[i][0], 'expunge':True})
-          
-     
+
+    # STEP: Rewrite the HAProxy config file if there is a change
+    if changeVMNum != 0:
+       # Get network IP addresses for currently running VMs
+       vmresult = api.listVirtualMachines({'region': vdcRegion, 'zone': zoneID, 'state': 'Running', 'name': vmName})
+       vmAddressList = [[vm['name'].replace('-','').lower(), [net for net in vm['nic'] if net['networkid']==networkIDs.split(',')[0]][0]['ipaddress']] for vm in vmresult['virtualmachine']]
+       vmAddressList.sort(key=lambda x: x[0])
+       ##print(vmAddressList)
+       with open(haproxyConfigFileStatic) as fh:
+          haproxyConfigStatic = fh.read()
+       with open(haproxyConfigFile, 'w') as outfh:
+          outfh.write(haproxyConfigStatic)
+          for v in vmAddressList:
+             outfh.write('\n        server %s %s:80 check' % (v[0], v[1]))
+       print("New configuration written to config file %s" % haproxyConfigFile)
+
+       # Restart the HAProxy service
+       call(['sudo', 'service', 'haproxy', 'restart'])
+       print("Service haproxy restarted.")
+
     # STEP: Program finished
     timeNow = datetime.datetime.utcnow()
     timestamp = timeNow.strftime("%Y-%m-%d %H:%M:%S UTC")
@@ -164,40 +216,3 @@ if __name__ == '__main__':
 
 
     
-'''
-if(($currentvms < $tobevms) and ($currentvms < $maxvms)) {
-	$addvms = $tobevms-$currentvms;
-	print "Increasing VM amount plus $addvms\n";
-	for (my $n=1;$n<=$addvms;$n++) {
-		my $randomnr = int(rand(8999)+1000);
-		my $command = "$cloudmonkey deploy virtualmachine name=$vmname$randomnr displayname=$vmname$randomnr zoneid=$zoneid serviceofferingid=$serviceofferingid templateid=$templateid networkids=$networkid > \/dev\/null 2>\&1";
-		# print "$command \n";
-		my $pid = fork();
-		if (defined $pid && $pid == 0) {
-			close STDIN;
-			close STDOUT;
-    			exec($command);
-    			exit 0;
-		}
-	}
-} elsif (($currentvms > $tobevms) and ($currentvms > $minvms)) {
-	$decvms = $currentvms-$tobevms;
-	print "Decreasing VM amount by $decvms\n";
-	for (my $n=1;$n<=$decvms;$n++) {
-		my $command = "$cloudmonkey stop virtualmachine id=$existingvms[$n-1] >\/dev\/null 2>\&1;$cloudmonkey destroy virtualmachine expunge=true id=$existingvms[$n-1] >\/dev\/null 2>\&1;$cloudmonkey stop virtualmachine id=$existingvms[$n-1] >\/dev\/null 2>\&1;$cloudmonkey destroy virtualmachine expunge=true id=$existingvms[$n-1] >\/dev\/null 2>\&1";
-		# print "$command\n";
-		my $pid = fork();
-		if (defined $pid && $pid == 0) {
-			close STDIN;
-			close STDOUT;
-			exec($command);
-			exit 0;
-		}
-	}
-} else {
-	print "Nothing to do\n";
-}
-
-print "################################\n";
-
-'''
